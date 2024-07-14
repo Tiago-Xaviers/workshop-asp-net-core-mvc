5880964e6c4a2454901ca732c6b2dac94a12aa1c
Tiago Xavier <tiago_xaviers@hotmail.com>
2024-07-14 11:48:40 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -6,15 +6,15 @@ namespace SalesWebMvc.Models
 {
     public class Department
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department () {}
 
-        public Department(int iD, string name)
+        public Department(int id, string name)
         {
-            ID = iD;
+            Id = id;
             Name = name;
         }
 
